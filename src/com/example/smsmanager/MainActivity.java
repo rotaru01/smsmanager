63623d0c4c6a817dd3ff03c1cb2f3d7c2acb347b
Gabi <gabi@yahoo.com>
2013-09-15 22:39:56 +0300

    adaugare actiune pe buton

--- src/com/example/smsmanager/MainActivity.java
+++ src/com/example/smsmanager/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.smsmanager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,15 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button buton = (Button)findViewById(R.id.button1);
+		buton.setOnClickListener(new OnClickListener(){
+
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Toast.makeText(MainActivity.this,"CANNA MEsaj Butonul merge:P",Toast.LENGTH_LONG).show();
+			}
+			
+		});
 	}
 
 	@Override
