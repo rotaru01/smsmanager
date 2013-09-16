ead79119ea76a9a4efe9f7bd63f006e8985a2f7c
canna <canna@yahoo.com>
2013-09-16 16:39:39 +0300

    Spinner pentru selectie grup

--- src/com/example/smsmanager/AddContactActivity.java
+++ src/com/example/smsmanager/AddContactActivity.java
@@ -10,13 +10,9 @@ public class AddContactActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_contact);
+		
+		
+		
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.add_contact, menu);
-		return true;
-	}
-
+	
 }
